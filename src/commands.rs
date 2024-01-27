bf66ec6246072233441368b145244b3e6f6d83fe
cophilot <philxsb@gmail.com>
2024-01-27 18:38:59 +0100

    update: Sat 27 Jan 2024 18:38:59 CET

--- src/commands.rs
+++ src/commands.rs
@@ -6,9 +6,33 @@ pub fn init() {
         return;
     }
     std::fs::create_dir(".templates").unwrap();
-    std::fs::write(".templates/README.md", crate::data::get_readme_content()).unwrap();
+    std::fs::write(
+        ".templates/README.md",
+        crate::data::get_init_readme_content(),
+    )
+    .unwrap();
+    std::fs::create_dir(".templates/ExampleTemplate").unwrap();
+    std::fs::write(
+        ".templates/ExampleTemplate/.templify",
+        crate::data::get_init_example_templify_content(),
+    )
+    .unwrap();
+    std::fs::write(
+        ".templates/ExampleTemplate/index.html",
+        crate::data::get_init_example_index_content(),
+    )
+    .unwrap();
 }
 
 pub fn help() {
-    println!("help...");
+    println!("templify help");
+    println!("");
+    println!("Usage: tmy <command>");
+    println!("");
+    println!("Commands:");
+    println!("  [ help | h ]              Show this help message");
+    println!("  [ init | i ]              Initialize Templify in your project");
+    println!("  [ new | n ] <name>        Create a new template with the given name");
+    println!("  [ list | l ]              List all templates");
+    println!("  [ generate | g ] <name>   Generate a new file from the given template");
 }
